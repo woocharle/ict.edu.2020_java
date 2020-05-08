8e4161b3d3de188b4df042ae1c99e28bfffa0782
woocharle <silvershots@naver.com>
2020-05-08 17:01:34 +0900

    2020/05/08 afternoon

--- 00_acmicpc/Main.java
+++ 00_acmicpc/Main.java
@@ -5,55 +5,62 @@ public class Main{
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		String str = br.readLine();
+		int n = Integer.parseInt(str);
+		int[] num = new int[n];
+		String[] res = new String[n];
 		
-		int n = 1;
-		int a;
-		String str_t; 
-		
-		do {
-			String str = br.readLine();
-			int input = Integer.parseInt(str);
-			a = input;
-			if (a != 0){
+		for (int i = 0; i < n; i++) {
+			str = br.readLine();
+			num[i] = Integer.parseInt(str);
+			//int cnt = 0;
+			int min = 0;
+			int max = 0;
+			
+			// 에라토스테네스의 체
+			int[] chk = new int[num[i] + 1];
+				
+			for(int j = 0; j < chk.length ; j++) {
+				if(j < 2) {
+					chk[j] = 0;
+				}
+				else {
+					chk[j] = j;
+				}
+			}
+			
+			for(int j = 2; j < chk.length ; j++) {				
+				for(int k = 2 * j ; k < chk.length; k += j) {
+					chk[k] = 0;
+				}
 				
 			}
+			// 거르기 완료
+			for(int j = 0; j <chk.length; j++) {
+				if(chk[j] == 0) {
+					continue;
+				}
+				if(chk[j] != 0) {
+					//cnt++;
+					// option
+					if(chk[j] <= num[i] / 2) {
+						min = chk[j];
+					}
+					if(chk[j] == num[i] - min) {
+						max = chk[j];
+						break;
+					}
+					
+				}
+			}
 			
-		} while(a != 0);
-		
-		//System.out.println(str_t);
-
-		
-		
-
-		/*
-		// 에라토스테네스의 체 
-		
-		int[] chk = new int[b + 1];
-			
-		for(int i = 0; i <= b ; i++) {
-			if(i < 2) {
-				chk[i] = 0;
-			}
-			else {
-				chk[i] = i;
-			}
+			res[i] = Integer.toString(min) + " " + Integer.toString(max);
 		}
 		
-		for(int i = 2; i <= b; i++) {
-			for(int j = 2 * i ; j <= b; j += i) {
-				chk[j] = 0;
-			}
+		for (int i = 0; i < n; i++) {
+			System.out.println(res[i]);
 		}
-		
-		// 거르기 완료
-		
-		for(int i = a; i <= b; i++) {
-			if(chk[i] != 0) {
-				System.out.println(chk[i]);
-			}
-		
-		}
-		*/
+	
 	}
 
 }
