fe59d1cc8bb92ec8b3a36c58b46773106435906e
woocharle <silvershots@naver.com>
2020-05-08 11:09:43 +0900

    2020/05/08 by JWC

--- 00_acmicpc/Main.java
+++ 00_acmicpc/Main.java
@@ -7,48 +7,37 @@ public class Main{
 		
 		String str = br.readLine();
 		int num = Integer.parseInt(str);
-		
-		int[] num_1 = new int[num]; //입력 데이터 층
-		int[] num_2 = new int[num]; //입력 데이터 호수
+		int[] res = new int [num]; 
 		
 		for(int i = 0; i < num ; i++) {
-			num_1[i] = Integer.parseInt(br.readLine());
-			num_2[i] = Integer.parseInt(br.readLine());
-			System.out.println(rule(num_1[i], num_2[i]));
-		}
-		
-	}
+				str = br.readLine();
+				String data[] = str.split(" "); 
+				int dep = Integer.parseInt(data[0]);
+				int arr = Integer.parseInt(data[1]);
+				int dis = arr - dep;
+				double move = 0;
+				res[i] = 0;
+				double cnt = 0;
+				
+				do{
+					cnt += 1; 
+					move = Math.pow(cnt, 2) + cnt;	
+					
+				}while(move >= dis);
 
-	public static int rule(int row, int col) {
-		
-		int sum = 0;
-		int[] num = new int[col];
-		int[] tmp = new int[col];
-		
-		for(int i = 0; i < col; i++) {
-			num[i] = i + 1;
-			tmp[i] = num[i];
+				if(move - cnt >= dis) {
+					cnt -= 1;
+				}
+			
+				
+				res[i] = (int)cnt;
+				
 		}
 		
-		for(int i = 0; i < row; i++) {
-			for (int j = 0; j < col; j++) {
-				sum = 0;
-				for(int k = 0; k <= j; k++) {
-					sum += tmp[k];
-				}	
-				num[j] = sum;
-				//System.out.println(num[j]);
-			}
-			for(int j = 0; j < col; j++) {
-				tmp[j] = num[j];
-			}
+		for (int i = 0; i < num; i++) {
+			System.out.println(res[i]);
+		}
 
-		}
-		
-		sum= num[col - 1];
-	
-		return sum;
-		
 	}
 
 }
